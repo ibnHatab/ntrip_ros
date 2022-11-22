3b691ecd6afeb0184b18c1c7c6b40d4a9ed08738
Vladyslav Kinzerskiy <vladyslav.kinzerskiy@porsche-engineering.ro>
2022-11-22 11:41:25 +0200

    restructure ros pkg

--- src/ntripclient.py
+++ src/ntripclient.py
@@ -6,7 +6,7 @@ import rospy
 from mavros_msgs.msg import RTCM
 
 import datetime
-from httplib import HTTPConnection
+from http.client import HTTPConnection
 from base64 import b64encode
 from threading import Thread
 
@@ -17,29 +17,57 @@ class ntripconnect(Thread):
         self.stop = False
 
     def run(self):
+        auth = str(self.ntc.ntrip_user) + ':' + self.ntc.ntrip_pass
         headers = {
             'Ntrip-Version': 'Ntrip/2.0',
             'User-Agent': 'NTRIP ntrip_ros',
             'Connection': 'close',
-            'Authorization': 'Basic ' + b64encode(self.ntc.ntrip_user + ':' + self.ntc.ntrip_pass)
+            'Authorization': 'Basic ' + str(b64encode(auth.encode('utf-8')))
         }
         connection = HTTPConnection(self.ntc.ntrip_server)
         now = datetime.datetime.utcnow()
-        connection.request('GET', '/'+self.ntc.ntrip_stream, self.ntc.nmea_gga % (now.hour, now.minute, now.second), headers)
-        
+        nmeadata = self.ntc.nmea_gga % (now.hour, now.minute, now.second)
+
+        csum = 0
+        for c in nmeadata:
+               # XOR'ing value of csum against the next char in line
+               # and storing the new XOR value in csum
+               if ord(c)!=',':
+                  csum ^= ord(c)
+
+        #convert hex characters to upper case
+        csum = hex(csum).upper()
+
+        #add 0x0 if checksum value is less than 0x10
+        if len(csum)==3:
+            csum='0'+csum[2]
+        else:
+            csum=csum[2:4]
+
+        nmeastring = '$'+nmeadata+'*'+csum+'\r\n'
+
+        connection.request('GET', '/'+self.ntc.ntrip_stream,nmeastring,headers)
+
         response = connection.getresponse()
         if response.status != 200: raise Exception("blah")
         buf = ""
         rmsg = RTCM()
+        r = rospy.Rate(10)
+
+        import pdb
+        pdb.set_trace()
+
         while not self.stop:
             data = response.read(1)
+            print('>>', data)
             if data!=chr(211):
                 continue
             l1 = ord(response.read(1))
             l2 = ord(response.read(1))
             pkt_len = ((l1&0x3)<<8)+l2
-    
+
             pkt = response.read(pkt_len)
+            print(pkt)
             parity = response.read(3)
             if len(pkt) != pkt_len:
                 rospy.logerr("Length error: {} {}".format(len(pkt), pkt_len))
@@ -48,14 +76,16 @@ class ntripconnect(Thread):
             rmsg.header.stamp = rospy.get_rostime()
             rmsg.data = data + chr(l1) + chr(l2) + pkt + parity
             self.ntc.pub.publish(rmsg)
+            r.sleep()
 
         connection.close()
+
 
 class ntripclient:
     def __init__(self):
         rospy.init_node('ntripclient', anonymous=True)
 
-        self.rtcm_topic = rospy.get_param('~rtcm_topic')
+        self.rtcm_topic = rospy.get_param('~rtcm_topic','/rtcm')
         self.nmea_topic = rospy.get_param('~nmea_topic', 'nmea')
 
         self.ntrip_server = rospy.get_param('~ntrip_server')
@@ -78,4 +108,3 @@ class ntripclient:
 if __name__ == '__main__':
     c = ntripclient()
     c.run()
-
